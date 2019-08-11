89af03287f115f6678aff1b4390055e04455e670
Engelbert Niehaus <niehaus@uni-landau.de>
2019-08-11 09:35:27 +0200

    Version update for package.json

--- src/npm_header.js
+++ src/npm_header.js
@@ -1,11 +1,11 @@
 /* ---------------------------------------
  Exported Module Variable: Handlebars4Code
  Package:  handlebars4code
- Version:  1.2.4  Date: 2019/08/11 9:21:02
+ Version:  1.2.6  Date: 2019/08/11 9:34:33
  Homepage: https://github.com/niebert/Handlebars4Code#readme
  Author:   Engelbert Niehaus
  License:  MIT
- Date:     2019/08/11 9:21:02
+ Date:     2019/08/11 9:34:33
  Inheritance: 'Handlebars4Code' inherits from 'Handlebars'
  Require Module with:
     const Handlebars4Code = require('handlebars4code');
